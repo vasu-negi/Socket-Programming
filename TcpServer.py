c475902d4903a0a7019745a207c36b37ee991251
vasu-negi <vasu.negi1995@gmail.com>
2020-04-07 23:46:10 -0400

    commit

--- TcpServer.py
+++ TcpServer.py
@@ -2,15 +2,14 @@ from socket import *
 
 server_port = 3933
 server_socket = socket(AF_INET,SOCK_STREAM)
-
 server_socket.bind(('',server_port))
 server_socket.listen(1)
 print("Server is ready to receive")
 while True:
     buf_size = 1024
     connection_socket,addres=  server_socket.accept()
-    message= server_socket.recv(buf_size)
+    message= connection_socket.recv(buf_size)
     modified_message = message.decode().upper()
     data = modified_message.encode()
-    server_socket.sendto(data)
+    connection_socket.send(data)
     connection_socket.close()
